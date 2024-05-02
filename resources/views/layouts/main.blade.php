9e041a779d0d62593506ae49c21ef4a7b02b92ba
shipkovandrej <shipkovandrej@yandex.ru>
2024-05-02 16:34:06 +0300

    fixed about scripts

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+    @yield('title')
+    <title>О компании</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="shortcut icon" type="image/ico" href="images/icons/favicon.ico"/>
+
+    <link rel="canonical" href="index.html"/>
+
+    <meta property="og:title" content="О компании"/>
+    <meta property="og:description" content=""/>
+    <meta property="og:image" content="https://www.tk-go.ru/images/logo.png"/>
+    <meta property="og:url" content="https://www.tk-go.ru/about/"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:site_name" content="Грузовое объединение"/>
+    <link rel="apple-touch-icon" href="../images/apple-touch-icon-60x60.png"/>
+    <link rel="apple-touch-icon" sizes="76x76" href="../images/apple-touch-icon-76x76.png"/>
+    <link rel="apple-touch-icon" sizes="120x120" href="../images/apple-touch-icon-120x120.png"/>
+    <link rel="apple-touch-icon" sizes="152x152" href="../images/apple-touch-icon-152x152.png"/>
+    <base href=""/>
+
+
+    @vite(['resources/css/style.css'])
+
+
+
+    <script src="{{ asset('js/top.js') }}"></script>
+    <script>
+        var serverfullname = "https://www.tk-go.ru/";
+        window.dataLayer = window.dataLayer || [];
+    </script>
+
+</head>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'about');
+Route::view('/', 'about')->name('about');
